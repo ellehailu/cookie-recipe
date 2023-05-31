c57f186ec590658b319ef94424f4a0c8869e8077
Student <hailu.elle@gmail.com>
2023-05-31 16:17:16 -0700

    add dark mode and light mode, updated buttons to respond accoringly Co-authored-by: Elle Hailu hailu.elle@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 // User Interface Logic
 window.onload = function() {
-    let h1 = document.querySelector("h1");
-    h1.onmouseover = function() {
-      window.alert("I am a heading element.");
-    };
-  
-    let p = document.querySelector("p");
-    p.onmouseover = function() {
-      document.querySelector("p>em").innerText = "Don't be surprised";
-    };
-  
-    let img = document.querySelector("img");
-    img.onmouseover = function() {
-      img.style.height = "700px";
-    };
-  };
+    let darkMode = document.querySelector(".btn2");
+    let body = document.body;
+    let button1 = document.querySelector(".btn1");
+    let button2 = document.querySelector(".btn2");
+    darkMode.onclick = function() {
+        body.style.background = 'black';
+        body.style.color = 'white'
+        button1.style.background = 'black';
+        button1.style.color = 'white';
+        button2.style.background = 'black';
+        button2.style.color = 'white';
+        
+    }
+    let lightMode = document.querySelector(".btn1");
+    lightMode.onclick = function() {
+        body.style.background = 'white';
+        body.style.color = 'black'
+        button2.style.background = 'white';
+        button2.style.color = 'black';
+        button1.style.background = 'white';
+        button1.style.color = 'black';
+    }
+ }
